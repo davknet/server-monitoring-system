0e84815b334ab2e2ac6fd1822e5fc2c722a3dd07
David <davknet1@gmail.com>
2026-03-26 19:41:39 +0200

    create request migrations

---
+++ database/migrations/2026_03_26_172810_create_request_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('request', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('server_id')->constrained()->cascadeOnDelete();
+            $table->boolean('is_success')->default(false);
+            $table->float('latency')->nullable(); // in milliseconds
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('request');
+    }
+};
