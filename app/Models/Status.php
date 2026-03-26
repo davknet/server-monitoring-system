c5831fe4e12abd831824f05da1a5027eaa67de1d
David <davknet1@gmail.com>
2026-03-26 19:38:55 +0200

    created status model

---
+++ app/Models/Status.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Status extends Model
+{
+
+     protected $table = 'statuses' ;
+      protected $fillable = [
+        'name'       ,
+        'description',
+        'is_active'
+    ];
+
+    
+}
