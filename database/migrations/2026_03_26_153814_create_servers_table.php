3315c39a0ca4a514eeca7199f2b1bf5534b800d2
David <davknet1@gmail.com>
2026-03-26 18:47:29 +0200

    create server migration

---
+++ database/migrations/2026_03_26_153814_create_servers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('servers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('url');
+            $table->foreignId('protocol_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('status_id')->constrained()->cascadeOnDelete();
+            $table->text('description')->nullable();
+            $table->json('config')->nullable(); // e.g. headers, auth details, etc.
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->timestamps();
+        
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('servers');
+    }
+};
