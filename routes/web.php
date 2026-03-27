8c977e750fc5a29f1d4adefca82d66a2ad28d428
David <davknet1@gmail.com>
2026-03-27 12:28:04 +0300

    remove route from api route and add it to web route and create home controller and home blade view and add login and logout routes to web.php and create auth controller with login and logout methods

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [App\Http\Controllers\Homecontroller::class, 'index']);
 
 
+Route::get('/login', [App\Http\Controllers\AuthController::class, 'showLogin']);
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+
+
 
 
