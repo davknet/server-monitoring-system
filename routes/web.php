bef1199f8867179915059fac59e8e29af66c375c
David <davknet1@gmail.com>
2026-03-27 21:41:28 +0300

    add ServerWebController to the routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\ServerController;
 use App\Http\Controllers\CreateServerController;
+use App\Http\Controllers\ServerWebController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,7 +11,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
     Route::get('/create-server', [CreateServerController::class, 'index'])->name('create-server');
-    Route::post('/add-server', [ServerController::class, 'store'])->name('servers.store');
+    Route::post('/add-server', [ServerWebController::class, 'store'])->name('servers.store');
 });
 
 
