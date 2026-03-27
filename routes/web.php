62734b42d7d1f30f2a5d94ecdb7a1c5263e0c5aa
David <davknet1@gmail.com>
2026-03-27 19:04:20 +0300

    make changes in route web

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 Route::middleware('auth')->group(function () {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-    Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+    Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+
+
 
 
 });
 
 
 
-Route::get('/login', [App\Http\Controllers\AuthController::class, 'showLogin']);
+Route::get('/login', [App\Http\Controllers\AuthController::class, 'showLogin'])
+    ->middleware('guest')
+    ->name('login');;
 
-Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])
+    ->middleware('guest');
 
 
 
