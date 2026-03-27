b65b11b3f327724f1f10621ed3715617ed5b491e
David <davknet1@gmail.com>
2026-03-27 14:55:53 +0300

    feat: make unauthrosied login rediect to login page

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
 
-Route::get('/', [App\Http\Controllers\Homecontroller::class, 'index']);
+
+});
+
 
 
 Route::get('/login', [App\Http\Controllers\AuthController::class, 'showLogin']);
+
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
-Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+
 
 
 
