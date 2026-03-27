87167ef0e62b30c2acbbff5d1ce32d60cf62f0eb
David <davknet1@gmail.com>
2026-03-27 19:08:29 +0300

    create route createServer new server

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
+    Route::get('/create-server', [App\Http\Controllers\CreateServerController::class, 'index'])->name('create-server');
 
 
 
