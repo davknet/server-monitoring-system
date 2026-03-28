a4495e2144e2b4cd1bfe1c7d34956c70259c328e
David <davknet1@gmail.com>
2026-03-28 12:37:33 +0300

    create:  update-server route

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Route::middleware('auth')->group(function () {
     Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
     Route::get('/create-server', [CreateServerController::class, 'index'])->name('create-server');
     Route::post('/add-server', [ServerWebController::class, 'store'])->name('servers.store');
+        Route::get('/update-server', [CreateServerController::class, 'update'])->name('update-server');
 });
 
 
