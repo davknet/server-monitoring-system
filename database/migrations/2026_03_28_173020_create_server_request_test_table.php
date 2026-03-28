7933dcf9714c2606c28c17be946aca616a35ede0
David <davknet1@gmail.com>
2026-03-28 20:32:27 +0300

    create request tes table migration

---
+++ database/migrations/2026_03_28_173020_create_server_request_test_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('server_request_test', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('server_id')->constrained()->cascadeOnDelete();
+            $table->string('server_name');
+            $table->string('server_ip');
+            $table->enum('status', ['healthy', 'not healthy']);
+            $table->text('message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('server_request_test');
+    }
+};
